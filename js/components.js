b8988e1bc1909a137dad5b5cd20bedc1658363fd
Jasmine Ghazarian <jasmine.ghazarian@example.com>
2025-09-16 15:13:25 +0400

    Обновлена структура программы курса и навигация
    - Объединены разделы по неделям в единые программы для всех модулей
    - Сокращено количество блоков с 12 до 8 во всех модулях
    - Изменен термин 'ճարտարապետություն' на 'արխիտեկտուրա'
    - Удален подзаголовок 'Մանրամասն ծրագիր 8 դասի համար'
    - Добавлен единообразный заголовок 'Դասընթացի ծրագիր' во все модули
    - Упрощено меню навигации - все пункты в одну строку
    - Исправлена структура HTML для корректного отображения

--- js/components.js
+++ js/components.js
@@ -23,9 +23,9 @@ class ComponentLoader {
                     </a>
                     <div class="nav-menu">
                         <a href="index.html" class="nav-link" data-page="home">Գլխավոր</a>
-                        <a href="module1.html" class="nav-link" data-page="module1">I մոդուլ<br>AI Explorer</a>
-                        <a href="module2.html" class="nav-link" data-page="module2">II մոդուլ<br>AI Developer</a>
-                        <a href="module3.html" class="nav-link" data-page="module3">III մոդուլ<br>AI Product Engineer</a>
+                        <a href="module1.html" class="nav-link" data-page="module1">I մոդուլ</a>
+                        <a href="module2.html" class="nav-link" data-page="module2">II մոդուլ</a>
+                        <a href="module3.html" class="nav-link" data-page="module3">III մոդուլ</a>
                         <a href="contact.html" class="nav-link" data-page="contact">Կապ</a>
                     </div>
                     <div class="hamburger" id="mobile-menu-toggle">
@@ -56,15 +56,15 @@ class ComponentLoader {
                         </a>
                         <a href="module1.html" class="mobile-menu-link" data-page="module1">
                             <i class="fas fa-search"></i>
-                            <span>I մոդուլ<br>AI Explorer</span>
+                            <span>I մոդուլ</span>
                         </a>
                         <a href="module2.html" class="mobile-menu-link" data-page="module2">
                             <i class="fas fa-code"></i>
-                            <span>II մոդուլ<br>AI Developer</span>
+                            <span>II մոդուլ</span>
                         </a>
                         <a href="module3.html" class="mobile-menu-link" data-page="module3">
                             <i class="fas fa-rocket"></i>
-                            <span>III մոդուլ<br>AI Product Engineer</span>
+                            <span>III մոդուլ</span>
                         </a>
                         <a href="contact.html" class="mobile-menu-link" data-page="contact">
                             <i class="fas fa-envelope"></i>
@@ -93,9 +93,9 @@ class ComponentLoader {
                         </a>
                         <div class="footer-links">
                             <a href="index.html">Գլխավոր</a>
-                            <a href="module1.html">I մոդուլ<br>AI Explorer</a>
-                            <a href="module2.html">II մոդուլ<br>AI Developer</a>
-                            <a href="module3.html">III մոդուլ<br>AI Product Engineer</a>
+                            <a href="module1.html">I մոդուլ</a>
+                            <a href="module2.html">II մոդուլ</a>
+                            <a href="module3.html">III մոդուլ</a>
                             <a href="contact.html">Կապ</a>
                         </div>
                         <div class="footer-social">
